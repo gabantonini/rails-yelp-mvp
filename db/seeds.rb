7877d926add01483ccb2ff921d88cf8e0a9fc51e
Gabriel Antonini <gabriel.antonini@gmail.com>
2020-04-23 17:28:13 -0300

    Seed done with 5 restaurant with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database'
+Restaurant.destroy_all
+
+puts 'Creating 5 restaurants...'
+
+5.times do
+  attributes = {
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: %w[chinese italian japanese french belgian].sample
+  }
+  restaurant = Restaurant.create!(attributes)
+  puts " Created restaurant #{restaurant.name} with ID #{restaurant.id}"
+end
+
+puts '5 restaurants have been created successfully!'
